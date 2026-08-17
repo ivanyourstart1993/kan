0532ecc00232d0422d0003f3868148f1a7c1a05e
ivanyourstart1993 <yourstart.com.ua@gmail.com>
2026-08-17 14:19:22 +0200

    Add chatRouter to appRouter

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,6 +1,7 @@
 import { attachmentRouter } from "./routers/attachment";
 import { boardRouter } from "./routers/board";
 import { cardRouter } from "./routers/card";
+import { chatRouter } from "./routers/chat";
 import { checklistRouter } from "./routers/checklist";
 import { feedbackRouter } from "./routers/feedback";
 import { healthRouter } from "./routers/health";
@@ -19,6 +20,7 @@ export const appRouter = createTRPCRouter({
   attachment: attachmentRouter,
   board: boardRouter,
   card: cardRouter,
+  chat: chatRouter,
   checklist: checklistRouter,
   feedback: feedbackRouter,
   health: healthRouter,
